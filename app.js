6f68558dab62ea3c013d0084406f9bc8c2c4dab2
Jason Lestina <coda19@gmail.com>
2017-09-09 00:54:10 -0400

    more server setup

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const express = require('express')
-const app = express()
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+var http           = require('http').Server(app);
+var dotenv         = require('dotenv');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// configuration ===========================================
 
-app.post('/scores', function(req, res) {
-  
-})
+//load environment variables,
+//either from .env files (development),
+//heroku environment in production, etc...
+dotenv.load();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+//parsing
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); //for parsing url encoded
+
+// routes
+require('./app/routes/routes')(app);
+
+//port for Heroku
+app.set('port', (process.env.PORT || 5000));
+
+//START ===================================================
+http.listen(app.get('port'), function(){
+  console.log('listening on port ' + app.get('port'));
+});
