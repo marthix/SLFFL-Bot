418478942fe266c36e184810bb38d3ceafc397cb
Jason Lestina <coda19@gmail.com>
2017-09-09 00:44:50 -0400

    adding scores route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.post('/scores', function(req, res) {
+  
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
